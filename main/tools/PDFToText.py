c8539247b92437dfa688c172188e438c66017986
TheGreatMarksman <151799337+TheGreatMarksman@users.noreply.github.com>
2025-09-02 17:17:50 -0700

    Delete main/tools/PDFToText.py

--- main/tools/PDFToText.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("Robinson_Pierpont_GNT.pdf") as pdf:
-    with open("Robinson_Pierpont_GNT_Attempt.txt", "w", encoding="utf-8") as out_file:
-        for page in pdf.pages:
-            text = page.extract_text()
-            if text:
-                out_file.write(text + "\n")
