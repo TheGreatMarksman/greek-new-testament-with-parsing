fdaf8c15d7187b338d05546ccee01d999ed60390
unknown <marcusedooley@yahoo.com>
2026-05-22 10:01:46 -0700

    visual

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 async function loadCSV() {
   const response = await fetch("/greek_new_testament_with_parsing/greek_new_testament_with_parsing.csv");
-  console.log("Fetch response:", response);
+  // console.log("Fetch response:", response);
   const text = await response.text();
-  console.log("CSV text:", text);
+  // console.log("CSV text:", text);
 
   const rows = text.trim().split("\n").map(r => r.split(","));
 
